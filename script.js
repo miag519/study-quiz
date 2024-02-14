4b1beb1913e42b844aedd4ea9e3c8823ab09e479
miag519 <miag1984.5@gmail.com>
2024-02-14 12:46:10 -0800

    uploading online

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+$(document).ready(function(){
+  $("button").click(function(){
+	$(this).siblings().filter(".correct").css({background": "green"});
+	$(this).siblings().filter(".answeroption").css({removeClass("grey"});
+  });
+   $(".answeroption").click(function(){
+    $(this).toggleClass("grey");
+  });
 
+ 
+  
+  
+  
+});
